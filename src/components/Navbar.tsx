3f1b1d94026e89013e3e976f7fcd3424a567fd30
Ethan Hao <hao.yz.ethan@gmail.com>
2024-04-27 21:42:58 -0400

    style navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,25 +2,30 @@ import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Nav = styled.nav`
-  background: #333;
-  color: #fff;
-  padding: 10px 20px;
-  display: flex;
-  justify-content: space-between;
+    background: #fff;
+    color: #333;
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 `;
 
 const NavItem = styled(NavLink)`
-  color: #fff;
-  text-decoration: none;
-  margin-right: 15px;
+    color: #333;
+    text-decoration: none;
+    margin-right: 15px;
+    font-family: 'Helvetica Neue', sans-serif;
+    font-size: 16px;
 
-  &.active {
-    font-weight: bold;
-  }
+    &.active {
+        font-weight: bold;
+        color: #e60023;
+    }
 
-  &:hover {
-    text-decoration: underline;
-  }
+    &:hover {
+        color: #e60023;
+        text-decoration: underline;
+    }
 `;
 
 const Navbar = () => {
