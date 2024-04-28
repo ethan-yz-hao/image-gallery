43defc0bedbf3ed89e17685514ae3c896d951cb6
Ethan Hao <hao.yz.ethan@gmail.com>
2024-04-28 15:27:20 -0400

    build about page

--- README.md
+++ README.md
@@ -1,19 +1,38 @@
 # Image Gallery
 
-This project is a web application built with React and TypeScript that allows users to view, sort, select, and download images. It utilizes Redux for state management and Styled Components for custom styled elements, providing a responsive and interactive user experience.
+This web application is built with React and TypeScript and allows users to interactively view, sort, select, and download images. Utilizing Redux for state management and Styled Components for custom styled elements, it offers a responsive and engaging user experience.
+
+Deployed on Vercel: [Image Gallery](https://image-gallery-eta.vercel.app/)
 
 ## Features
-- image gallery with infinite scroll, mansory layout
-- loading skeleton
-- image sorting (by date, title, ascending, descending)
-- image search (by title or description)
-- image selection (mutilple selection, select all, clear all, dropdown list for bulk actions)
-- responsive layout
+- **Infinite Scrolling Image Gallery**: Displays images using a masonry layout that supports infinite scrolling.
+- **Loading Skeleton**: Provides a placeholder loading skeleton while images are being fetched.
+- **Image Sorting**: Users can sort images by date and title, in both ascending and descending orders.
+- **Image Search**: Includes a search functionality to filter images by title or description.
+- **Image Selection**: Supports multiple selections, select all, clear all, and a dropdown for bulk actions.
+- **Responsive Layout**: Adapts smoothly from desktop to mobile screens, with dynamic adjustments in the utility bar for different devices.
 
 ## Technologies
-- Styled Components, ReactIcon, react mansory css
-- react router dom
-- react redux
+
+- **Styled Components & React Icons**: For styling and iconography.
+- **React Masonry CSS**: For creating an optimal masonry layout of images.
+- **React Router Dom**: For routing capabilities within the application.
+- **React Redux**: For managing global state, specifically for tracking selected images.
 
 ## Installation
 
+1. Clone the repository and navigate to the project directory.
+```
+git clone https://github.com/ethan-yz-hao/image-gallery.git
+cd image-gallery
+```
+
+2. Install the dependencies.
+```
+npm install
+```
+
+3. Start the development server.
+```
+npm run dev
+```
--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,7 +1,103 @@
-const AboutPage = () => {
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    max-width: 800px;
+    margin: auto;
+    padding: 20px;
+    color: #333;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`;
+
+const Title = styled.h1`
+    font-size: 24px;
+    color: #e60023;
+`;
+
+const Subtitle = styled.h2`
+    font-size: 18px;
+    color: #e60023;
+`;
+
+const Text = styled.p`
+    font-size: 16px;
+    text-align: left;
+    line-height: 1.5;
+`;
+
+const List = styled.ul`
+    padding-left: 20px;
+`;
+
+const ListItem = styled.li`
+    text-align: left;
+    margin-bottom: 10px;
+`;
+
+const Command = styled.code`
+    background: #f5f5f5;
+    border-radius: 4px;
+    padding: 4px 8px;
+    font-family: monospace;
+    text-align: left;
+`;
+
+const AboutPage: React.FC = () => {
     return (
-        <div>
-            About Page
+        <div style={{
+            width: '100%',
+            position: 'fixed',
+            top: '45px'
+        }}>
+            <Container>
+                <Title>Image Gallery</Title>
+                <Text>This web application is built with React and TypeScript and allows users to interactively view,
+                    sort,
+                    select, and download images. Utilizing Redux for state management and Styled Components for custom
+                    styled elements, it offers a responsive and engaging user experience.</Text>
+
+                <Subtitle>Features</Subtitle>
+                <List>
+                    <ListItem><b>Infinite Scrolling Image Gallery:</b> Displays images using a masonry layout that
+                        supports
+                        infinite scrolling.</ListItem>
+                    <ListItem><b>Loading Skeleton:</b> Provides a placeholder loading skeleton while images are being
+                        fetched.</ListItem>
+                    <ListItem><b>Image Sorting:</b> Users can sort images by date and title, in both ascending and
+                        descending
+                        orders.</ListItem>
+                    <ListItem><b>Image Search:</b> Includes a search functionality to filter images by title or
+                        description.</ListItem>
+                    <ListItem><b>Image Selection:</b> Supports multiple selections, select all, clear all, and a
+                        dropdown for
+                        bulk
+                        actions.</ListItem>
+                    <ListItem><b>Responsive Layout:</b> Adapts smoothly from desktop to mobile screens, with dynamic
+                        adjustments in
+                        the utility bar for different devices.</ListItem>
+                </List>
+
+                <Subtitle>Technologies</Subtitle>
+                <List>
+                    <ListItem><b>Styled Components & React Icons:</b> For styling and iconography.</ListItem>
+                    <ListItem><b>React Masonry CSS:</b> For creating an optimal masonry layout of images.</ListItem>
+                    <ListItem><b>React Router Dom:</b> For routing capabilities within the application.</ListItem>
+                    <ListItem><b>React Redux:</b> For managing global state, specifically for tracking selected
+                        images.</ListItem>
+                </List>
+
+                <Subtitle>Installation</Subtitle>
+                <Text>1. Clone the repository and navigate to the project directory.</Text>
+                <Command>git clone https://github.com/ethan-yz-hao/image-gallery.git</Command>
+                <Command>cd image-gallery</Command>
+                <Text>2. Install the dependencies.</Text>
+                <Command>npm install</Command>
+                <Text>3. Start the development server.</Text>
+                <Command>npm run dev</Command>
+            </Container>
         </div>
     );
 };
